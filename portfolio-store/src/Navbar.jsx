cad8fe1196f8bf766d0ff0589f9cc964482bd473
draginojd <arminfazlikhan@gmail.com>
2024-07-21 15:32:03 +0200

    hamburger menu strucutre added

--- portfolio-store/src/Navbar.jsx
+++ portfolio-store/src/Navbar.jsx
@@ -12,9 +12,39 @@ function Navbar() {
       <div className="shopping-cart-container">
         <img src="/shopping-cart.svg" alt="shopping cart icon" className="shopping-cart" width="50" height="50"/>
       </div>
+
+
       <div className="burger-container">
-        <img src="/burgericon.svg" alt="burger sidebar button mobile" className="hamburger" width="50" height="50" />
+        <input type="checkbox" id="burger-checkbox" className="burger-checkbox"/>
+        <img src="/burgericon.svg" alt="burger sidebar button mobile" width="50" height="50" className="hamburger"/>
+  
+        <div className="hamburger-menu">
+          <div className="hamburger-menu-link-container">
+           <img src="#" alt="home img" className="hambruger-menu-home" />   <a href="#" className="hamburger-menu-link">HOME</a>
+          </div> 
+          <div className="hamburger-menu-link-container">
+            <img src="#" alt="home img" className="hambruger-menu-home" />   <a href="#" className="hamburger-menu-link">HOME</a>
+          </div> 
+          <div className="hamburger-menu-link-container">
+            <img src="#" alt="home img" className="hambruger-menu-home" />   <a href="#" className="hamburger-menu-link">HOME</a>
+          </div> 
+          <div className="hamburger-menu-link-container">
+            <img src="#" alt="home img" className="hambruger-menu-home" />   <a href="#" className="hamburger-menu-link">HOME</a>
+          </div> 
+          <div className="hamburger-menu-link-container">
+            <img src="#" alt="home img" className="hambruger-menu-home" />   <a href="#" className="hamburger-menu-link">HOME</a>
+          </div> 
+          <div className="hamburger-menu-link-container">
+            <img src="#" alt="home img" className="hambruger-menu-home" />   <a href="#" className="hamburger-menu-link">HOME</a>
+          </div> 
+          <div className="hamburger-menu-link-container">
+            <img src="#" alt="home img" className="hambruger-menu-home" />   <a href="#" className="hamburger-menu-link">HOME</a>
+        </div> 
+      
+
+        </div>
       </div>
+
     </nav> 
   );
 };
