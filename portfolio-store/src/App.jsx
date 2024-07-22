b8f5ca29099d27e7ce9a0ef3ddb89c37a10038ea
root <root@vmi2011325.contaboserver.net>
2024-07-22 16:09:38 +0200

    Fixed back /portfolio deleted files

--- portfolio-store/src/App.jsx
+++ portfolio-store/src/App.jsx
@@ -1,29 +1,24 @@
-
+import React from 'react'; // Add necessary imports
 import './App.css';
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import Navbar from "./Navbar"
+// import { useEffect, useState } from 'react'; // Comment out if not used
+import axios from 'axios';
+import Navbar from './Navbar';
 
 function App() {
   const sendPostRequest = async () => {
     try {
-      const response = await axios.post("http://localhost:4000");
+      const response = await axios.post(`${process.env.REACT_APP_API_URL}/`);
       console.log(response.data);
     } catch (error) {
-      console.error("There was and ERROR!" , error);
+      console.error('There was an ERROR!', error);
     }
   };
 
   return (
     <div className="App">
       <Navbar />
-    <header className="App-header">
-       
-    
-    <button onClick={sendPostRequest}>Send POST Request</button>
-    
-      </header>
-    </div> 
+      <button onClick={sendPostRequest}>Send Post Request</button>
+    </div>
   );
 }
 
