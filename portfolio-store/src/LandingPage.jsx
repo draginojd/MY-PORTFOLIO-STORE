b93ef7a21ea18cbf027b35d8e17a55db6d7edb23
draginojd <arminfazlikhan@gmail.com>
2024-07-23 17:26:11 +0200

    minor improvments hamburger menu

---
+++ portfolio-store/src/LandingPage.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import React from "react";
+import axios from "axios";
+
+
+function LandingPage() {
+    return(
+        <div className="landing-page-container">
+
+        </div>
+    );
+};
+
+export default LandingPage;
